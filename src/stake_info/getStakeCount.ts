18d1fb5f9c2dbbf5017bf7b240d4a0e58ecf3abe
kot-shrodingera <kot.shrodingera@gmail.com>
2020-09-07 13:57:27 +0600

    getStakeCount: добавлено определение маленьких купонов

--- src/stake_info/getStakeCount.ts
+++ src/stake_info/getStakeCount.ts
@@ -1,7 +1,7 @@
 import getStakeCountGenerator from '@kot-shrodingera-team/germes-generators/stake_info/getStakeCount';
 
 const getStakeCount = getStakeCountGenerator({
-  stakeElementSelector: '.stake-wide--1XHB_',
+  stakeElementSelector: '.stake-wide--1XHB_, .stake-narrow--3wROh',
 });
 
 export default getStakeCount;
