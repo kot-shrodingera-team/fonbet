db8e23fa51fd11bd4cf664394bcd47cc51770288
kot-shrodingera <kot.shrodingera@gmail.com>
2021-03-09 16:58:51 +0600

    fix(afterSuccesfullLogin): ожидание API перед проверкой аккаунта

--- src/initialization/afterSuccesfulLogin.ts
+++ src/initialization/afterSuccesfulLogin.ts
@@ -1,7 +1,14 @@
+import { awaiter, log } from '@kot-shrodingera-team/germes-utils';
 import processCookieModalWindow from './processCookieModalWinow';
 import { checkAccountBlocked, accountBlocked } from './accountChecks';
+import appLoaded from './appLoaded';
 
 const afterSuccesfulLogin = async (): Promise<void> => {
+  await awaiter(appLoaded);
+  if (!appLoaded()) {
+    log('API не загрузилось', 'crimson');
+    return;
+  }
   processCookieModalWindow();
   if (checkAccountBlocked()) {
     accountBlocked();
