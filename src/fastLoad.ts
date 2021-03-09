df440f6f59840b470dc1cef06c97fc9091c86a3c
kot-shrodingera <kot.shrodingera@gmail.com>
2021-03-09 17:00:10 +0600

    feat(fastLoad): обновление перехода на лайв

--- src/fastLoad.ts
+++ src/fastLoad.ts
@@ -1,4 +1,4 @@
-import { log, checkUrl } from '@kot-shrodingera-team/germes-utils';
+import { log, checkUrl, getElement } from '@kot-shrodingera-team/germes-utils';
 import { version } from '../package.json';
 import showStake from './show_stake';
 import { clearMinimumStake } from './stake_info/getMinimumStake';
@@ -16,9 +16,25 @@ const fastLoad = async (): Promise<void> => {
   }
   const { pathname } = window.location;
   if (!pathname.startsWith('/live') && !pathname.startsWith('/bets')) {
-    log('Не на странице линии или лайва. Переходим на лайв', 'orange');
-    window.location.href = new URL('/live', worker.BookmakerMainUrl).href;
-    return;
+    log('Не на странице линии или лайва', 'steelblue');
+    const liveButton = document.querySelector('[href="/live"]') as HTMLElement;
+    if (!liveButton) {
+      log('Не найдена кнопка перехода на лайв', 'crimson');
+      log('Переходим на лайв по URL', 'orange');
+      window.location.href = new URL('/live', worker.BookmakerMainUrl).href;
+      return;
+    }
+    log('Переходим на лайв по кнопке', 'orange');
+    liveButton.click();
+    const betsTable = await getElement(
+      '[class^="line-filter-layout__content"]'
+    );
+    if (!betsTable) {
+      log('Не найдена таблица ставок', 'red');
+      worker.JSFail();
+      return;
+    }
+    log('Появилась таблица ставок', 'steelblue');
   }
   clearMinimumStake();
   clearMaximumStake();
