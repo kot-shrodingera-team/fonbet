d3de5ab03d65029108af6ab6605714bfecb8d952
Kot Shrodingera <kot.shrodingera@gmail.com>
2021-12-17 18:51:47 +0100

    delete(afterSuccesfulStake): сброс флага couponOpening (не используется)

--- src/initialization/afterSuccesfulLogin.ts
+++ src/initialization/afterSuccesfulLogin.ts
@@ -3,9 +3,6 @@ import { checkAccountBlocked, accountBlocked } from '../helpers/accountChecks';
 import appLoaded from '../helpers/checkAppLoaded';
 
 const afterSuccesfulLogin = async (): Promise<void> => {
-  // Сброс флага активности открытия купона, если было какое-то зависание
-  localStorage.setItem('couponOpening', '0');
-
   await awaiter(appLoaded);
   if (checkAccountBlocked()) {
     accountBlocked();
